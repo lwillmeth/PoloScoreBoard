9d1a37fbc1d0cedddd555be70001741006895b36
Levi Willmeth <levi.willmeth@gmail.com>
2017-04-16 20:58:28 -0700

    Simplify scoring code by moving more functionality into shiftOut()

--- scoreboard/scoreboard.ino
+++ scoreboard/scoreboard.ino
@@ -46,15 +46,14 @@ void setup() {
   dataArray[9] = 0b01011111;
 
   // all on for 1 second
-  digitalWrite(LATCHPIN, 0);
-  shiftOut(DATAPIN, CLOCKPIN, 0x00);
-  digitalWrite(LATCHPIN, 1);
+  shiftOut(0x00);
   delay(1000);
 
   leftScore = 0;
 }
 
 void loop() {
+/*
   // Check if left button is being pressed
   if(prevLeftPress + DEBOUNCE < millis() && !digitalRead(ButtonLeftPin)){
     leftScore = (leftScore + 1) % 10; // increment 1, range is 0-9 inc
@@ -66,26 +65,16 @@ void loop() {
   shiftOut(DATAPIN, CLOCKPIN, leftScore);
   digitalWrite(LATCHPIN, 1);
   delay(100);
+*/
 
-  /*
   for (int j = 0; j < 10; j++) {
-    //load the light sequence you want from array
-    data = dataArray[j];
-    //ground LATCHPIN and hold low for as long as you are transmitting
-    digitalWrite(LATCHPIN, 0);
-    shiftOut(DATAPIN, CLOCKPIN, data);
-    //return the latch pin high to signal chip that it 
-    //no longer needs to listen for information
-    digitalWrite(LATCHPIN, 1);
+    shiftOut(dataArray[j]);
     delay(1000);
   }
 
-  // all off for 3 seconds
-  digitalWrite(LATCHPIN, 0);
-  shiftOut(DATAPIN, CLOCKPIN, 0x00);
-  digitalWrite(LATCHPIN, 1);
+  // All off for 3 seconds
+  shiftOut(0x00);
   delay(3000);
-  */
 }
 
 /*
@@ -96,75 +85,32 @@ void incLeftScore(){
 */
 
 
-// the heart of the program
-void shiftOut(int myDATAPIN, int myCLOCKPIN, byte score) {
-  // This shifts 8 bits out MSB first, 
-  //on the rising edge of the clock,
-  //clock idles low
-  score = dataArray[score];
-
-  //internal function setup
-  int i=0;
-  int pinState;
-  pinMode(myCLOCKPIN, OUTPUT);
-  pinMode(myDATAPIN, OUTPUT);
-
-  //clear everything out just in case to
-  //prepare shift register for bit shifting
-  digitalWrite(myDATAPIN, 0);
-  digitalWrite(myCLOCKPIN, 0);
-
-  //for each bit in the byte score�
+void shiftOut(byte score) {
+  digitalWrite(LATCHPIN, 0);
+  
   //NOTICE THAT WE ARE COUNTING DOWN in our for loop
   //This means that %00000001 or "1" will go through such
   //that it will be pin Q0 that lights. 
-  for (i=7; i>=0; i--)  {
-    digitalWrite(myCLOCKPIN, 0);
+  for (byte i=7; i>=0; i--)  {
+    digitalWrite(CLOCKPIN, 0);
 
     //if the value passed to score and a bitmask result 
     // true then... so if we are at i=6 and our value is
     // %11010100 it would the code compares it to %01000000 
     // and proceeds to set pinState to 1.
-    if ( score & (1<<i) ) {
-      pinState= 1;
-    }
-    else {  
-      pinState= 0;
-    }
+//    if ( score & (1<<i) ) {
+//      pinState= 1;
+//    }
+//    else {  
+//      pinState= 0;
+//    }
 
     //Sets the pin to HIGH or LOW depending on pinState
-    digitalWrite(myDATAPIN, pinState);
+    digitalWrite(DATAPIN, (score & (1<<i))?1:0);
     //register shifts bits on upstroke of clock pin  
-    digitalWrite(myCLOCKPIN, 1);
-    //zero the data pin after shift to prevent bleed through
-    digitalWrite(myDATAPIN, 0);
+    digitalWrite(CLOCKPIN, 1);
   }
 
   //stop shifting
-  digitalWrite(myCLOCKPIN, 0);
-}
-
-
-//blinks the whole register based on the number of times you want to 
-//blink "n" and the pause between them "d"
-//starts with a moment of darkness to make sure the first blink
-//has its full visual effect.
-void blinkAll_2Bytes(int n, int d) {
-  digitalWrite(LATCHPIN, 0);
-  shiftOut(DATAPIN, CLOCKPIN, 0);
-  shiftOut(DATAPIN, CLOCKPIN, 0);
   digitalWrite(LATCHPIN, 1);
-  delay(200);
-  for (int x = 0; x < n; x++) {
-    digitalWrite(LATCHPIN, 0);
-    shiftOut(DATAPIN, CLOCKPIN, 255);
-    shiftOut(DATAPIN, CLOCKPIN, 255);
-    digitalWrite(LATCHPIN, 1);
-    delay(d);
-    digitalWrite(LATCHPIN, 0);
-    shiftOut(DATAPIN, CLOCKPIN, 0);
-    shiftOut(DATAPIN, CLOCKPIN, 0);
-    digitalWrite(LATCHPIN, 1);
-    delay(d);
-  }
 }
