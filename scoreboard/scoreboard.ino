e305e78a76afa7ac54e7d5dbfd95b82a2e656aeb
Levi Willmeth <levi.willmeth@gmail.com>
2017-04-23 21:40:16 -0700

    Adjust pins to match new pcb.

--- scoreboard/scoreboard.ino
+++ scoreboard/scoreboard.ino
@@ -6,11 +6,11 @@
  * TPIC6B596 shift register 
  * LEDs attached to each of the outputs of the shift register `
  */
+#define DATAPIN 13      // SER IN (Serial In) on 596
 #define LATCHPIN 12     // RCK (Register Clock) on 596
 #define CLOCKPIN 11     // SRCK (Shift-Register Clock) on 596
-#define DATAPIN 10      // SER IN (Serial In) on 596
-#define REDBUTTON 9     // Red button
-#define BLUEBUTTON 8    // Blue button
+#define REDBUTTON 10     // Red button
+#define BLUEBUTTON 9    // Blue button
 #define DEBOUNCE 500    // Min delay between button presses
 
 //holders for infromation you're going to pass to shifting function
@@ -20,7 +20,8 @@ byte digits[10];
 // prevent button debounce
 long gameTimer, prevRedPress, prevBluePress;
 
-void setup() {
+void setup()
+{
   //set pins to output because they are addressed in the main loop
   pinMode(LATCHPIN, OUTPUT);
   pinMode(CLOCKPIN, OUTPUT);
@@ -36,16 +37,16 @@ void setup() {
   Serial.println("Bike polo scoreboard starting up..");
 
   // The binary patterns for each decimal digit
-  digits[0] = 0b11111110;
-  digits[1] = 0b00001100;
-  digits[2] = 0b01110111;
-  digits[3] = 0b00111111;
-  digits[4] = 0b10001101;
-  digits[5] = 0b10111011;
-  digits[6] = 0b11111011;
-  digits[7] = 0b00001110;
-  digits[8] = 0b11111111;
-  digits[9] = 0b10111111;
+  digits[0] = 0b1111110;
+  digits[1] = 0b0001100;
+  digits[2] = 0b0110111;
+  digits[3] = 0b0011111;
+  digits[4] = 0b0001101;
+  digits[5] = 0b0111011;
+  digits[6] = 0b1111011;
+  digits[7] = 0b0001110;
+  digits[8] = 0b1111111;
+  digits[9] = 0b0111111;
   digits[100] = 0x00;
   digits[0xFF] = 0x00;
 
@@ -66,14 +67,15 @@ void setup() {
   pushLED(blueScore);
 }
 
-void loop() {
-
+void loop()
+{
   // Check if red button is being pressed
   if(prevRedPress + DEBOUNCE < millis() && !digitalRead(REDBUTTON)){
-    redScore = (redScore + 1) % 10; // increment 1, range is 0-9 inc
+    redScore = (redScore + 1) % 6; // increment 1, range is 0-5 inc
     if(redScore==0 && blueScore==0){
       // If both teams change to a zero score, reset the game.
       // all on for 1 second
+      gameTimer = millis();
       pushLED(0xFF);
       pushLED(0xFF);
       delay(1000);
@@ -88,10 +90,11 @@ void loop() {
 
     // Check if blue button is being pressed
   if(prevBluePress + DEBOUNCE < millis() && !digitalRead(BLUEBUTTON)){
-    blueScore = (blueScore + 1) % 10; // increment 1, range is 0-9 inc
+    blueScore = (blueScore + 1) % 6; // increment 1, range is 0-5 inc
     if(redScore==0 && blueScore==0){
       // If both teams change to a zero score, reset the game.
       // all on for 1 second
+      gameTimer = millis();
       pushLED(0xFF);
       pushLED(0xFF);
       delay(1000);
@@ -115,31 +118,10 @@ void loop() {
       pushLED(blueScore);
     }
   }
-
-//  for (int j = 0; j < 10; j++){
-//    pushLED(j);
-//    pushLED((10-j)%10);
-//    delay(1000);
-//  }
-//
-//  // All off for 3 seconds
-//  pushLED(0x00);
-//  pushLED(0x00);
-//  delay(1500);
-//  pushLED(0xFF);
-//  pushLED(0xFF);
-//  delay(1500);
 }
 
-/*
-void incredScore(){
-    data = (data + 1) % 10; // increment 1, range is 0-9 inc
-    Serial.println(data);
-}
-*/
-
-
-void pushLED(byte score) {
+void pushLED(byte score)
+{
   Serial.println(score);
   digitalWrite(LATCHPIN, 0);
   //NOTICE WE ARE COUNTING DOWN
