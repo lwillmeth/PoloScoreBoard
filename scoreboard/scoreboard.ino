25dc4ba9fca2489a7a02c28ce7c9d82ef35fd518
Levi Willmeth <levi.willmeth@gmail.com>
2017-04-24 14:34:25 -0700

    Simplify duplicated statement, fix bug in 15 min timer.

--- scoreboard/scoreboard.ino
+++ scoreboard/scoreboard.ino
@@ -9,9 +9,10 @@
 #define DATAPIN 13      // SER IN (Serial In) on 596
 #define LATCHPIN 12     // RCK (Register Clock) on 596
 #define CLOCKPIN 11     // SRCK (Shift-Register Clock) on 596
-#define REDBUTTON 10     // Red button
+#define REDBUTTON 10    // Red button
 #define BLUEBUTTON 9    // Blue button
 #define DEBOUNCE 500    // Min delay between button presses
+#define ENABLEPIN A0    // G' on 596, HIGH=LEDs enabled, LOW=disabled
 
 //holders for infromation you're going to pass to shifting function
 byte redScore, blueScore;
@@ -26,12 +27,14 @@ void setup()
   pinMode(LATCHPIN, OUTPUT);
   pinMode(CLOCKPIN, OUTPUT);
   pinMode(DATAPIN, OUTPUT);
+  pinMode(ENABLEPIN, OUTPUT);
   pinMode(REDBUTTON, INPUT_PULLUP);
   pinMode(BLUEBUTTON, INPUT_PULLUP);
 
   digitalWrite(LATCHPIN, LOW);
   digitalWrite(CLOCKPIN, LOW);
   digitalWrite(DATAPIN, LOW);
+  digitalWrite(ENABLEPIN, LOW);
 
   Serial.begin(9600);
   Serial.println("Bike polo scoreboard starting up..");
@@ -41,8 +44,8 @@ void setup()
   digits[1] = 0b0001100;
   digits[2] = 0b0110111;
   digits[3] = 0b0011111;
-  digits[4] = 0b0001101;
-  digits[5] = 0b0111011;
+  digits[4] = 0b1001101;
+  digits[5] = 0b1011011;
   digits[6] = 0b1111011;
   digits[7] = 0b0001110;
   digits[8] = 0b1111111;
@@ -69,55 +72,41 @@ void setup()
 
 void loop()
 {
-  // Check if red button is being pressed
-  if(prevRedPress + DEBOUNCE < millis() && !digitalRead(REDBUTTON)){
-    redScore = (redScore + 1) % 6; // increment 1, range is 0-5 inc
-    if(redScore==0 && blueScore==0){
-      // If both teams change to a zero score, reset the game.
-      // all on for 1 second
-      gameTimer = millis();
-      pushLED(0xFF);
-      pushLED(0xFF);
-      delay(1000);
-    }
-    // Either way, push the new scores
-    Serial.println(redScore);
-    pushLED(redScore);
-    pushLED(blueScore);
-    // reset red button timer
-    prevRedPress = millis();
-  }
+  // Check if red button is being pressed and handle it
+  checkButton(REDBUTTON, &redScore, &prevRedPress);
 
-    // Check if blue button is being pressed
-  if(prevBluePress + DEBOUNCE < millis() && !digitalRead(BLUEBUTTON)){
-    blueScore = (blueScore + 1) % 6; // increment 1, range is 0-5 inc
-    if(redScore==0 && blueScore==0){
-      // If both teams change to a zero score, reset the game.
-      // all on for 1 second
-      gameTimer = millis();
-      pushLED(0xFF);
-      pushLED(0xFF);
-      delay(1000);
-    }
-    // Either way, push the new scores
-    Serial.println(blueScore);
-    pushLED(redScore);
-    pushLED(blueScore);
-    // reset blue button timer
-    prevBluePress = millis();
-  }
+  // Check if blue button is being pressed and handle it
+  checkButton(BLUEBUTTON, &blueScore, &prevBluePress);
 
   // Blink scoreboard when game time runs out
   if(millis() - gameTimer > 900000){
     // 15 minute games, after that blink the scores every half second
     if(millis() % 1000 > 500){
-      pushLED(100);
-      pushLED(100);
+      digitalWrite(ENABLEPIN, LOW);
     } else {
-      pushLED(redScore);
-      pushLED(blueScore);
+      digitalWrite(ENABLEPIN, HIGH);
     }
   }
+}
+
+void checkButton(byte BUTTON, byte *score, long *buttonTimer)
+{
+    if(*buttonTimer + DEBOUNCE < millis() && !digitalRead(BUTTON)){
+      *score = (*score + 1) % 6; // increment 1, range is 0-5 inc
+      if(redScore==0 && blueScore==0){
+        // If both teams change to a zero score, reset the game.
+        gameTimer = millis();
+        digitalWrite(ENABLEPIN, HIGH);
+        delay(1000);
+        digitalWrite(ENABLEPIN, LOW);
+      }
+      // Either way, push the new scores
+      // Serial.println(score);
+      pushLED(redScore);
+      pushLED(blueScore);
+      // reset button timer
+      *buttonTimer = millis();
+    }
 }
 
 void pushLED(byte score)
